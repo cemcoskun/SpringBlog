3a471bb9796d743376bef8c75ccda49524975bd4
Cem COŞKUN <cemcoskun.bt@gmail.com>
2020-05-12 19:40:41 +0300

    comment approve

---
+++ SpringBlog/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpringBlog.Areas.Admin.Controllers
+{
+    public class CommentsController : AdminBaseController
+    {
+        // GET: Admin/Comments
+        public ActionResult Index()
+        {
+            return View(db.Comments.OrderByDescending(x => x.CreationTime).ToList());
+        }
+        [HttpPost]
+        public ActionResult ChangeState(int id, bool isPublished)
+        {
+            var comment = db.Comments.Find(id);
+            comment.State = isPublished ? Enums.CommentState.Approved : Enums.CommentState.Rejected;
+            db.SaveChanges();
+
+            return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
